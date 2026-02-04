93ba430e4f8c62bb4cc0402027bd62465f76c019
Vrutti Kalani <72266844+vruttikalani30@users.noreply.github.com>
2026-02-05 01:07:10 +0530

    Update parabank.e2e.spec.ts

--- tests/ui/parabank.e2e.spec.ts
+++ tests/ui/parabank.e2e.spec.ts
@@ -10,20 +10,18 @@ import {
   BillPayPage
 } from '../../pages';
 
-test('ParaBank E2E UI Flow', async ({ page }) => {
+test('ParaBank full E2E flow (UI + Data)', async ({ page }) => {
   const user = generateUser();
-  await page.goto('/parabank/index.htm');
+  await page.goto('/index.htm');
 
   await new RegisterPage(page).register(user);
   await new LoginPage(page).login(user.username, user.password);
 
   await new HomePage(page).verifyGlobalMenu();
 
-  const openAccountPage = new OpenAccountPage(page);
-  const accountNumber = await openAccountPage.openSavingsAccount();
+  const accountId = await new OpenAccountPage(page).openSavingsAccount();
+  await new AccountsOverviewPage(page).verifyAccountVisible(accountId);
 
-  await new AccountsOverviewPage(page).validateBalance(accountNumber);
-
-  await new TransferFundsPage(page).transfer(accountNumber, accountNumber, '10');
-  await new BillPayPage(page).payBill(accountNumber, '5');
+  await new TransferFundsPage(page).transferFunds(accountId, accountId, '10');
+  await new BillPayPage(page).payBill(accountId, '5');
 });
