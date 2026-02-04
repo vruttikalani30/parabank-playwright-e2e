436b26ececc9ce743ee1419e99c1f5a1bc5354ff
Vrutti Kalani <72266844+vruttikalani30@users.noreply.github.com>
2026-02-05 01:05:41 +0530

    Update TransferFundsPage.ts

--- pages/TransferFundsPage.ts
+++ pages/TransferFundsPage.ts
@@ -3,13 +3,16 @@ import { Page, expect } from '@playwright/test';
 export class TransferFundsPage {
   constructor(private page: Page) {}
 
-  async transfer(from: string, to: string, amount: string) {
-    await this.page.click('text=Transfer Funds');
+  async transferFunds(from: string, to: string, amount: string) {
+    await this.page.click('a[href*="transfer.htm"]');
+
     await this.page.fill('#amount', amount);
     await this.page.selectOption('#fromAccountId', from);
     await this.page.selectOption('#toAccountId', to);
     await this.page.click('input[value="Transfer"]');
 
-    await expect(this.page.locator('text=Transfer Complete')).toBeVisible();
+    await expect(
+      this.page.locator('text=Transfer Complete!')
+    ).toBeVisible();
   }
 }
